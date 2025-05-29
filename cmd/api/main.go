99be38246959ddc32df06015a46043658f90d28e
mihiryadav20 <80512042+mihiryadav20@users.noreply.github.com>
2025-05-29 16:58:00 +0530

    CORS added

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 	"github.com/mihiryadav20/goauth/internal/auth"
@@ -46,6 +47,16 @@ func main() {
 
 	// Use logger middleware
 	app.Use(logger.New())
+
+	// Configure CORS
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3001",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
+		ExposeHeaders:    "Content-Length",
+		MaxAge:           86400, // 24 hours
+	}))
 
 
 	// Initialize services
